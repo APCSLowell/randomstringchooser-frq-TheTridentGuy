c1533444a4f07452d5557705edecc9f57e35ba00
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-04-25 23:54:20 -0700

    I HOPE YOUR FUCKING NULL POINTER EXCEPTIONS GO TO HELL

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -2,7 +2,18 @@ import java.util.*;
 public class RandomStringChooser
 {
   /* to be implemented in part (a) */
-  
+    ArrayList<String> strings = new ArrayList<String>();
+  public RandomStringChooser(String[] strs){
+      for(String str : strs){
+          strings.add(str);
+      }
+  }
+  public String getNext(){
+      if(strings.size() == 0){
+          return "NONE";
+      }
+      return strings.remove((int)(Math.random() * strings.size()));
+  }
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
 }
